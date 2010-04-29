c7c5f51f80e5f441886797334541050662f6379b
Daniel at ITV impa's virtual host <daniel.dalb@gmail.com>
2010-04-29 19:18:37 -0300

    first orbit and db source code test

---
+++ orb/db.lua
@@ -0,0 +1,49 @@
+
+local dado = require "dado"
+local db = dado.connect ("ndoutils", "ndoutils", "itv", "mysql")
+
+--db.conn:execute ("select * from nagios_hosts") -- direct use of LuaSQL
+
+select_hosts = function()
+	for field1, field2 in db:select ("host_id, alias", "nagios_hosts", "host_id >= 11") do
+    		print(field1, field2)
+	end
+end
+
+select_all_hosts = function()
+	-- melhor uso: create table
+	assert ( pcall( db.assertexec, db, "select * from nagios_hosts"))
+end
+
+select_full_path_tree = function()
+	select = [[ 
+		SELECT node.name
+		FROM nested_category AS node,
+		nested_category AS parent
+		WHERE node.lft BETWEEN parent.lft AND parent.rgt
+		AND parent.name = 'ELECTRONICS'
+		ORDER BY node.lft;
+	]]
+end
+
+select_hosts()
+select_all_hosts()
+
+
+t = db:selectall ("host_id, alias", "nagios_hosts", "host_id >= 11")
+for i, v in ipairs(t) do
+	print(v.host_id, v.alias )
+end
+
+
+t = db:selectall ("h.host_id, h.alias, o.object_id ", " nagios_hosts h, nagios_objects o "," h.host_object_id = o.object_id")
+for i, v in ipairs(t) do
+	print(v.host_id, v.alias, v.object_id )
+end
+
+
+
+db:close()
+
+
+
---
+++ orb/hello.lua
@@ -0,0 +1,27 @@
+#!/usr/bin/env wsapi.cgi
+
+module(..., package.seeall)
+
+function do_something()
+  local i = 14
+  local j = 3
+
+  return(tostring(i*j))
+
+end
+
+function run(wsapi_env)
+  local headers = { ["Content-type"] = "text/html" }
+
+  local function hello_text()
+    local s = do_something()
+    coroutine.yield("<html><body>")
+    coroutine.yield("<p>Hello Wsapi!</p>")
+    coroutine.yield("<p>PATH_INFO: " .. wsapi_env.PATH_INFO .. "</p>")
+    coroutine.yield("<p>SCRIPT_NAME: " .. wsapi_env.SCRIPT_NAME .. "</p>")
+    coroutine.yield("<p>DO SOMETHING: " .. s .. "</p>")
+    coroutine.yield("</body></html>")
+  end
+
+  return 200, headers, coroutine.wrap(hello_text)
+end
