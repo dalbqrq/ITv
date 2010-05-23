daff92ba39a99c90700002a4fa9c712703404deb
Daniel at ITV impa's virtual host <daniel.dalb@gmail.com>
2010-05-23 04:35:41 -0300

    input forms on orbit

--- orb/app.lua
+++ orb/app.lua
@@ -25,17 +25,33 @@ end
 -- controllers
 
 function index(web)
-  return render_index()
+   return render_index()
 end
-
-function say(web, name, n1, n2)
-  return render_say(web, name, n1, n2)
-end
-
--- dispatchers
 
 itvision:dispatch_get(index, "/", "/index")
+
+
+function say(web, name, n1, n2)
+   return render_say(web, name, n1, n2)
+end
+
 itvision:dispatch_get(say, "/say/(%a+):(%d+):(%d+)")
+
+
+function add(web, post_id)
+   local input = web.imput
+   --return web:redirect(web:link("/post/" .. post_id))
+   return render_add(web, post_id, web.input.user)
+end
+
+itvision:dispatch_post(add, "/add/(%d+)")
+
+
+function input(web)
+   return render_input(web)
+end
+
+itvision:dispatch_get(input, "/input")
 
 -- views
 
@@ -52,8 +68,6 @@ function render_itvision()
    for i,v in ipairs(a) do
       s = s.."[ "..v.app_id.." + "..v.object_id.." ] <p>"
    end
---[[
-]]
    return p.itvision"Hello World!"..s
 end
 
@@ -65,6 +79,46 @@ function render_say(web, name, n1, n2)
    return render_layout(render_itvision() .. 
      p.itvision((web.input.greeting or "Hello ") .. name .. " : " .. n1 .." : " .. n2 .." !"))
 end
+
+function render_add(web, post_id, user)
+   return render_layout(p.itvision((web.input.greeting or "Hello ") .. post_id .." & ".. user))
+end
+
+function render_input(web)
+   res = {}
+
+   res[#res + 1] = form{
+      name = "input",
+      method = "post",
+      action = "/add/30"),
+      --action = web:link("/add/30"),
+
+      --p{ strings.form_name, br(), input{ type="text", name="author", value = web.input.author },
+      p{ "NAME:", br(), input{ type="text", name="author", value = "Daniel" },
+         br(), br(),
+
+--[[
+         strings.form_email, br(), input{ type="text", name="email",
+         value = web.input.email },
+         br(), br(),
+         strings.form_url, br(), input{ type="text", name="url",
+         value = web.input.url },
+         br(), br(),
+         strings.comments .. ":", br(), err_msg,
+         textarea{ name="comment", rows="10", cols="60", web.input.comment },
+         br(),
+         em(" *" .. strings.italics .. "* "),
+         strong(" **" .. strings.bold .. "** "),
+         " [" .. a{ href="/url", strings.link } .. "](http://url) ",
+         br(), br(),
+]]
+         input.button{ type="submit", value="Enviar" }
+      }
+   }
+   res[#res + 1] = p.itvision "INPUT"
+   return render_layout(res)
+end
+
 
 orbit.htmlify(itvision, "render_.+")
 
