1cab571d2d6a2042912e05361f86c6c0f214a92a
root <root@itvision.localdomain>
2010-10-31 12:45:19 -0200

    fix no script install.sh

--- ks/install.sh
+++ ks/install.sh
@@ -2,17 +2,19 @@
 # --------------------------------------------------
 # ITVISION
 # --------------------------------------------------
-itvhome=/usr/local/itv
-user=$USER
+user=itv
+pass=itv
+itvhome=/usr/local/itvision
 
 mkdir $itvhome
 chown $user.$user $itvhome
-if [ "$1" == "clone-repo-with-ssh" ]; then # SSH-KEY NUST BE INCLUDED IN GITHUB
-   ssh-keygen -t rsa -C "daniel@itvision.com.br" -N "" -f ~$user/.ssh/id_rsa > /dev/null
+if [ "$1" == "clone-for-devel" ]; then # MUST USE PASSWORD OR SSH-KEY MUST BE INCLUDED IN GITHUB
+   ssh-keygen -t rsa -C "devel@itvision.com.br" -N "" -f ~$user/.ssh/id_rsa > /dev/null
    sudo -u $user git clone git@github.com:dlins/ITv.git $itvhome
 else
    sudo -u $user git clone git://github.com/dlins/ITv.git $itvhome
 fi
-sed -i -e "s,^user=.*,user=$user,g" $itvhome/ks/itvision.sh
-sed -i -e "s,^itvhome=.*,itvhome=$itvhome,g" $itvhome/ks/itvision.sh
+sed -i -e "s,^user=.*,user=$user,g" \
+	-e "s,^dbpass=.*,dbpass=$pass,g" \
+	-e "s,^itvhome=.*,itvhome=$itvhome,g" $itvhome/ks/itvision.sh
 $itvhome/ks/itvision.sh
