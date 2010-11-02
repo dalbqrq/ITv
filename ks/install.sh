04567a0752765e6b1c77aac50b470ccdd0b1ff3e
Daniel Lins de Albuquerque <daniel.dalb@gmail.com>
2010-11-02 21:41:37 -0200

    acertos finais nos scripts de instalacao

--- ks/install.sh
+++ ks/install.sh
@@ -3,11 +3,16 @@
 # ITVISION
 # --------------------------------------------------
 itvhome=/usr/local/itv
-user=itv
+user=$USER
 
 mkdir $itvhome
 chown $user.$user $itvhome
-sudo -u $user git clone git://github.com/dlins/ITv.git $itvhome
+if [ "$1" == "clone-repo-with-ssh" ]; then # SSH-KEY NUST BE INCLUDED IN GITHUB
+   ssh-keygen -t rsa -C "daniel@itvision.com.br" -N "" -f ~$user/.ssh/id_rsa > /dev/null
+   sudo -u $user git clone git@github.com:dlins/ITv.git $itvhome
+else
+   sudo -u $user git clone git://github.com/dlins/ITv.git $itvhome
+fi
 sed -i -e "s,^user=.*,user=$user,g" $itvhome/ks/itvision.sh
 sed -i -e "s,^itvhome=.*,itvhome=$itvhome,g" $itvhome/ks/itvision.sh
 $itvhome/ks/itvision.sh
