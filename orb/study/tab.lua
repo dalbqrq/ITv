df2adaa7342719fbb249f9d585142c3aea5ca000
Daniel Lins de Albuquerque <daniel@itvision.com.br>
2010-11-15 22:57:04 -0200

    TODO e estudo de tabelas

--- orb/study/tab.lua
+++ orb/study/tab.lua
@@ -1,5 +1,43 @@
+--[[
+
+   +-----------+    +-------------------------+    +-----------------+      +----------+
+   |  glpi_    |----| glpi_                   |----|  glpi_          |------|  glpi_   |
+   | COMPUTER  |    |COMPUTER_SOFTWAREVERSION |    | SOFTWAREVERSION |      | SOFTWARE |
+   +-----------+    +-------------------------+    +-----------------+      +----------+
+         |                                                 |
+         |                                                 |
+   +-------------+                                    +-----------+
+   |  glpi_      |------------------------------------| itvision_ |
+   | NETWORKPORT |                                    | MONITOR   |
+   +-------------+                                    +-----------+
+                                                           |
+                                                           |
+                                                      +---------------+
+   +-------------+         +-------------+            | nagios_       |
+   | itvision_   |---------| itvision_   |----------- | OBJECTS  .or. |
+   | apps        |         | app_objects |            | SERVICES .or. |
+   +-------------+         +-------------+            | SERVICESTATUS |
+                                                      +---------------+
+
+]]
+
 
 require "Model"
+
+local all_tables = { 
+	"itvision_apps",
+	"itvision_app_objects",
+	"itvision_app_monitor",
+	"nagios_objects",
+	"nagios_services",
+	"nagios_servicestatus",
+	"glpi_computers",
+	"glpi_networkequipments",
+	"glpi_networkports",
+	"glpi_software",
+	"glpi_software_version",
+	
+}
 
 local t = show_columns("itvision_app")
 
