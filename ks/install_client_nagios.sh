4bba7af50720c906d91304d9abcdea4e665e8142
Ricardo Gomes <rgomes@impa.br>
2010-12-21 10:40:01 -0200

    changes install_client_nagios

--- ks/install_client_nagios.sh
+++ ks/install_client_nagios.sh
@@ -23,7 +23,7 @@ useradd -s /bin/false $USERNAGIOS
 if [ $RELEASE == "CentOS" ]; then
 	rpm -ivh /usr/local/itvision/ks/nrpe-2.12-12.el5.x86_64.rpm
 	mkdir -p /usr/local/nagios/libexec/
-	tar -xzf /usr/local/itvision/ks/plugins.tar.gz -C /usr/local/nagios/libexec/
+	#tar -xzf /usr/local/itvision/ks/plugins.tar.gz -C /usr/local/nagios/libexec/
 	chown -R nagios.nagios /usr/local/nagios/
 
 elif [ $RELEASE == "Ubuntu" ]; then
@@ -41,12 +41,16 @@ sed -i.orig -e "s/nrpe_user=nrpe/nrpe_user=$USERNAGIOS/" \
 
 sed -i '/^command/Id' /etc/nagios/nrpe.cfg
 sed -i '/^#command/Id' /etc/nagios/nrpe.cfg
+PROC=(`ps aux |wc -l`)
+WARPROC=(`expr $PROC + 100`)
+CRITPROC=(`expr $PROC + 200`)
+
 cat << EOF >> /etc/nagios/nrpe.cfg
 command[check_load]=/usr/local/nagios/libexec/check_load -w 15,10,5 -c 30,25,20
 command[check_mem]=/usr/local/nagios/libexec/check_mem -f -w 20 -c 10 -C
 command[check_swap]=/usr/local/nagios/libexec/check_swap -w 30% -c 10%
 command[check_zombie_procs]=/usr/local/nagios/libexec/check_procs -w 5 -c 10 -s Z
-command[check_total_procs]=/usr/local/nagios/libexec/check_procs -w 150 -c 200 
+command[check_total_procs]=/usr/local/nagios/libexec/check_procs -w $WARPROC -c $CRITPROC
 EOF
 
 DISK=(`df -h |awk -F " " '{print $1}' |awk -F "/" '{print $3}' |sed '/^$/d'`)
@@ -69,7 +73,7 @@ chkconfig nrpe on
 #SUBJECT="Nova Monitacao Nagios $SHNAME"
 #EMAIL="netadm@impa.br"
 #echo "O servidor $SHNAME foi reinstalado, favor verificar a nova monitoração para os serviços no servidor Magiros" > /tmp/message.txt
-#grep ^command nrpe.cfg |awk -F "check" '{print $3}' |sed -e 's/_//g' >> /tmp/message.txt
+#grep ^command /etc/nagios/nrpe.cfg |awk -F "check" '{print $3}' |sed -e 's/_//g' >> /tmp/message.txt
 #/bin/mail -s "$SUBJECT" "$EMAIL" < /tmp/message.txt
 #
 #rm -f /tmp/message.txt
