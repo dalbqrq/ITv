97ac82f52b09d0756933a79903358a7ee3e02ede
Daniel Lins de Albuquerque <daniel.dalb@gmail.com>
2011-03-18 09:31:34 -0300

    remocao de ip sob nova chave de host

--- scr/update_ip.lua
+++ scr/update_ip.lua
@@ -21,41 +21,30 @@ end
 
 function delete_ip_address(id)
    print("delete: "..id)
-
-   local a = Model.query("itvision_app_objects ao, itvision_monitors m", 
-                         "ao.service_object_id = m.service_object_id and name1 = "..id,
-                         nil, "distinct(app_id) as app_id")
    local m = Model.query("itvision_monitors", "networkports_id = "..id)
 
    if m[1] then
-      --itvision_monitors
-      Model.delete ("itvision_monitors", "name1 = '"..id.."'")
-      --itvision_checkcmd_params
-      Model.delete ("itvision_checkcmd_params", "name1 = '"..id.."'")
-      --DEBUG: print("--> "..id)
+      hst_name = m[1].name1
 
-      --itvision_app_objects
-      --itvision_app_relats
-      for _,v in ipairs(m) do
-         Model.delete ("itvision_app_objects", "service_object_id = "..v.service_object_id)
-         Model.delete ("itvision_app_relats", "from_object_id = "..v.service_object_id)
-         Model.delete ("itvision_app_relats", "to_object_id = "..v.service_object_id)
-         --DEBUG: print("   m|-> ".. v.service_object_id)
-      end
+      local a = Model.query("itvision_app_objects ao, itvision_monitors m", 
+                         "ao.service_object_id = m.service_object_id and m.name1 = '"..hst_name.."'",
+                         nil, "distinct(app_id) as app_id")
 
-      --itvision_apps
+      Model.delete ("itvision_monitors", "name1 = '"..hst_name.."'")
+      Model.delete ("itvision_checkcmd_params", "name1 = '"..hst_name.."'")
+
       for _,v in ipairs(a) do
-         --DEBUG: print("   a|-> ".. v.app_id)
          local apps = Model.query("itvision_app_objects", "app_id = "..v.app_id, nil, "count(*) as count")
          if apps[1] and tonumber(apps[1].count) == 0 then
-            --DEBUG: print("   apps|-> ".. v.app_id)
             App.deactivate_app (v.app_id, 0)
          end
       end
 
       for _,v in ipairs(m) do
          local servicefile = config.monitor.dir.."/services/"..v.name1.."-"..v.name2..".cfg"
-         --DEBUG: print("   m|-> ".. v.name1.."-"..v.name2)
+         Model.delete ("itvision_app_objects", "service_object_id = "..v.service_object_id)
+         Model.delete ("itvision_app_relats", "from_object_id = "..v.service_object_id)
+         Model.delete ("itvision_app_relats", "to_object_id = "..v.service_object_id)
          remove_file(servicefile)
       end
 
