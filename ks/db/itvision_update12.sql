746dd9d972069595f53c6602757571a80a2c2783
Daniel Lins de Albuquerque <daniel.dalb@gmail.com>
2011-06-17 02:47:04 -0300

    fixes etc

---
+++ ks/db/itvision_update12.sql
@@ -0,0 +1,9 @@
+
+update itvision_checkcmd_default_params  set description = 'Tempo de resposta para alerta ANORMAL' where id = 11;
+update itvision_checkcmd_default_params  set description = 'Tempo de resposta para alerta CRÍTICO' where id = 12;
+update itvision_checkcmd_default_params  set description = 'Comando a ser executado no servidor' where id = 5;
+update itvision_checkcmd_default_params  set description = 'Diferença de tempo para alerta ANORMAL (segundos)' where id = 27;
+update itvision_checkcmd_default_params  set description = 'Diferença de tempo para alerta CRÍTICO (segundos)' where id = 28;
+update itvision_checkcmd_default_params  set description = 'Endereço a ser consultado' where id = 22;
+
+
