7354f3005c63d4aa2446cba060ef6d282cbbfc6d
Daniel Lins de Albuquerque <daniel@itvision5.itvision.com.br>
2012-05-21 19:15:34 -0700

    Renomeacao dos submenus de relat_menu.lua

--- orb/relat_menu.lua
+++ orb/relat_menu.lua
@@ -1,7 +1,7 @@
 module("relat_menu", package.seeall)
 
 submenu = {
-   { name="Relatório 1", field="reports", link="/servdesk/relatorios/index.php" },
-   { name="Relatório 2", field="reports", link="/servdesk/relatorios/relat_2.php" },
-   --{ name="Relatório 3", field="reports", link="/servdesk/relatorios/relat_3.php" },
+   { name="Ocorrências", field="reports", link="/servdesk/relatorios/index.php" },
+   { name="Relatório 1", field="reports", link="/servdesk/relatorios/relat_1.php" },
+   --{ name="Relatório 2", field="reports", link="/servdesk/relatorios/relat_2.php" },
 }
